7d71cf00f26235c11b27b55b64cba163a58b621d
Dmitrii-Sh <teufel1987@mail.ru>
2024-12-02 00:53:18 +0300

    staging create and update

--- airflow/dags/dag_utils/staging_queries.py
+++ airflow/dags/dag_utils/staging_queries.py
@@ -14,39 +14,39 @@ def create_stg_browser_events_table():
 
 
 def staging_update_query(execution_date):
-    f"""
-    BEGIN;
-
-    DELETE FROM public.stg_browser_events
-    WHERE event_timestamp >= '{execution_date}'
-        AND event_timestamp < CAST('{execution_date}' AS timestamp) + INTERVAL '1' HOUR;
-
-    WITH
-      data_truncate AS (
-        SELECT
-          click_id,
-          event_id,
-          event_timestamp::timestamp AS event_timestamp,
-          event_type,
-          browser_name,
-          browser_language,
-          browser_user_agent,
-          row_number() OVER (PARTITION BY click_id, event_id, event_timestamp,
-            event_type, browser_name, browser_language, browser_user_agent) AS rn
-        FROM public.browser_events
-        WHERE date_trunc('hour', event_timestamp::timestamp) = '{execution_date}')
-
-    SELECT
-      click_id,
-      event_id,
-      event_timestamp,
-      event_type,
-      browser_name,
-      browser_language,
-      browser_user_agent
-    FROM data_truncate
-    WHERE rn = 1
-    ;
-
-    COMMIT;
+    return f"""
+            BEGIN;
+        
+            DELETE FROM public.stg_browser_events
+            WHERE event_timestamp >= '{execution_date}'
+                AND event_timestamp < CAST('{execution_date}' AS timestamp) + INTERVAL '1' HOUR;
+        
+            WITH
+              data_truncate AS (
+                SELECT
+                  click_id,
+                  event_id,
+                  event_timestamp::timestamp AS event_timestamp,
+                  event_type,
+                  browser_name,
+                  browser_language,
+                  browser_user_agent,
+                  row_number() OVER (PARTITION BY click_id, event_id, event_timestamp,
+                    event_type, browser_name, browser_language, browser_user_agent) AS rn
+                FROM public.browser_events
+                WHERE date_trunc('hour', event_timestamp::timestamp) = '{execution_date}')
+        
+            SELECT
+              click_id,
+              event_id,
+              event_timestamp,
+              event_type,
+              browser_name,
+              browser_language,
+              browser_user_agent
+            FROM data_truncate
+            WHERE rn = 1
+            ;
+        
+            COMMIT;
 """
