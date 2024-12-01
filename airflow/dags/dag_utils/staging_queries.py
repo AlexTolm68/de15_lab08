a53ddf95de978f044d986e66ed345488052dfa85
Dmitrii-Sh <teufel1987@mail.ru>
2024-12-02 00:44:47 +0300

    staging create and update

---
+++ airflow/dags/dag_utils/staging_queries.py
@@ -0,0 +1,50 @@
+def create_stg_browser_events_table():
+    return """
+            CREATE TABLE IF NOT EXISTS public.stg_browser_events
+            (
+              click_id           VARCHAR(40),
+              event_id           VARCHAR(40),
+              event_timestamp    TIMESTAMP,
+              event_type         VARCHAR(10),
+              browser_name       VARCHAR(20),
+              browser_language   VARCHAR(10),
+              browser_user_agent TEXT
+            );
+            """
+
+
+def staging_update_query(execution_date):
+    """
+    BEGIN;
+
+    DELETE FROM public.stg_browser_events
+    WHERE event_timestamp >= '{execution_date}'
+        AND event_timestamp < CAST('{execution_date}' AS timestamp) + INTERVAL '1' HOUR;
+
+    WITH
+      data_truncate AS (
+        SELECT
+          click_id,
+          event_id,
+          event_timestamp::timestamp AS event_timestamp,
+          event_type,
+          browser_name,
+          browser_language,
+          browser_user_agent,
+          row_number() OVER (PARTITION BY click_id, event_id, event_timestamp,
+            event_type, browser_name, browser_language, browser_user_agent) AS rn
+        FROM public.browser_events
+        WHERE date_trunc('hour', event_timestamp::timestamp) = '{execution_date}')
+
+    SELECT
+      click_id,
+      event_id,
+      event_timestamp,
+      event_type,
+      browser_name,
+      browser_language,
+      browser_user_agent
+    FROM data_truncate
+    WHERE rn = 1
+    ;
+"""
---
+++ airflow/dags/staging_layer_dag.py
@@ -0,0 +1,65 @@
+import os
+import psycopg2
+import pendulum
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+from dag_utils.staging_queries import create_stg_browser_events_table, staging_update_query
+
+
+PG_USER = os.environ["POSTGRES_USER"]
+PG_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+PG_DATABASE = os.environ["POSTGRES_DB"]
+DEFAULT_ARGS = {"owner": "lab08_team"}
+
+CONNECTION_STRING = f'postgresql://{PG_USER}:{PG_PASSWORD}@postgres-db:5432/{PG_DATABASE}'  # 5432 внутри
+
+
+def postgres_execute_query(query: str) -> None:
+    with psycopg2.connect(CONNECTION_STRING) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+
+
+def execute_sql_stg_table_update(**kwargs):
+    postgres_execute_query(create_stg_browser_events_table())
+    postgres_execute_query(staging_update_query(kwargs['execution_date']))
+
+
+with DAG(
+    dag_id='data_marts_dag',
+    default_args=DEFAULT_ARGS,
+    schedule_interval="0 * * * *",
+    start_date=pendulum.datetime(2024, 11, 15),
+    catchup=False,
+) as dag:
+
+    wait_of_browser_events = ExternalTaskSensor(
+        task_id="wait_of_browser_events", external_dag_id="lab08_browser_events", external_task_id="grab_s3_data"
+    )
+    wait_of_device_events = ExternalTaskSensor(
+        task_id="wait_of_device_events", external_dag_id="lab08_device_events", external_task_id="grab_s3_data"
+    )
+    wait_of_geo_events = ExternalTaskSensor(
+        task_id="wait_of_geo_events", external_dag_id="lab08_geo_events", external_task_id="grab_s3_data"
+    )
+    wait_of_location_events = ExternalTaskSensor(
+        task_id="wait_of_location_events", external_dag_id="lab08_location_events", external_task_id="grab_s3_data",
+    )
+
+    staging_update = PythonOperator(
+        task_id='staging_update',
+        python_callable=execute_sql_stg_table_update,
+        provide_context=True,
+    )
+
+    start = EmptyOperator(task_id="start")
+    wait_for_dependencies = EmptyOperator(task_id="wait_for_dependencies")
+    completed = EmptyOperator(task_id="completed")
+
+    start >> wait_of_browser_events >> wait_for_dependencies
+    start >> wait_of_device_events >> wait_for_dependencies
+    start >> wait_of_geo_events >> wait_for_dependencies
+    start >> wait_of_location_events >> wait_for_dependencies
+    wait_for_dependencies >> staging_update >> completed
